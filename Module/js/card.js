0f779756c2afdeabb0343953cde360d59b9bb225
diyidilei <35208524+diyidilei@users.noreply.github.com>
2019-09-10 10:18:10 +0800

    Updated by CrossUI No-Code App Builder

--- Module/js/card.js
+++ Module/js/card.js
@@ -8,7 +8,10 @@ xui.Class('Module.card', 'xui.Module',{
         // Required modules
         Required:[],
         // To initialize properties
-        properties : {},
+        properties : {
+            "v_name > caption":null,
+            "v_age > caption":null
+        },
 
         // To initialize instance(e.g. properties)
         initialize : function(){
@@ -19,7 +22,61 @@ xui.Class('Module.card', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Block")
+                .setHost(host,"xui_ui_block10")
+                .setLeft("0em")
+                .setTop("0em")
+                .setWidth("23.333333333333332em")
+                .setHeight("9.166666666666666em")
+                .setBorderType("groove")
+            );
+            
+            host.xui_ui_block10.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label2")
+                .setLeft("2.4166666666666665em")
+                .setTop("2.3333333333333335em")
+                .setWidth("5.75em")
+                .setHeight("1.8333333333333333em")
+                .setCaption("姓名")
+                .setFontSize("18px")
+            );
+            
+            host.xui_ui_block10.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label3")
+                .setLeft("1.5em")
+                .setTop("4.833333333333333em")
+                .setWidth("6.25em")
+                .setHeight("2.6666666666666665em")
+                .setCaption("年龄")
+                .setFontSize("18px")
+            );
+            
+            host.xui_ui_block10.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"v_name")
+                .setLeft("10.666666666666666em")
+                .setTop("2.3333333333333335em")
+                .setWidth("7.916666666666667em")
+                .setHeight("1.8333333333333333em")
+                .setCaption("")
+                .setFontSize("18px")
+            );
+            
+            host.xui_ui_block10.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"v_age")
+                .setLeft("10.666666666666666em")
+                .setTop("4.833333333333333em")
+                .setWidth("7.083333333333333em")
+                .setHeight("1.8333333333333333em")
+                .setCaption("")
+                .setFontSize("18px")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
