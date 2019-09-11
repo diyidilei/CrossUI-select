af62da98a037d29229d6549431111573940e63c3
diyidilei <35208524+diyidilei@users.noreply.github.com>
2019-09-11 14:08:54 +0800

    Updated by CrossUI No-Code App Builder

--- Module/js/card.js
+++ Module/js/card.js
@@ -19,7 +19,7 @@ xui.Class('Module.card', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
