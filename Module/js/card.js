365400799ae2154ca2c87976d89083b88184f86b
diyidilei <35208524+diyidilei@users.noreply.github.com>
2019-09-11 14:15:48 +0800

    Updated by CrossUI No-Code App Builder

--- Module/js/card.js
+++ Module/js/card.js
@@ -20,6 +20,63 @@ xui.Class('Module.card', 'xui.Module',{
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
+            append(
+                xui.create("xui.UI.Block")
+                .setHost(host,"xui_ui_block6")
+                .setLeft("0em")
+                .setTop("0em")
+                .setWidth("22.5em")
+                .setHeight("9.166666666666666em")
+                .setPosition("relative")
+                .setDisplay("inline-block")
+                .setBorderType("none")
+            );
+            
+            host.xui_ui_block6.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label1")
+                .setLeft("5em")
+                .setTop("1.6666666666666667em")
+                .setWidth("3.9166666666666665em")
+                .setHeight("2em")
+                .setCaption("姓名")
+                .setFontSize("18px")
+            );
+            
+            host.xui_ui_block6.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label2")
+                .setLeft("3.3333333333333335em")
+                .setTop("5em")
+                .setWidth("5.583333333333333em")
+                .setHeight("2em")
+                .setCaption("年龄")
+                .setFontSize("18px")
+            );
+            
+            host.xui_ui_block6.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label3")
+                .setLeft("12.5em")
+                .setTop("1.6666666666666667em")
+                .setWidth("5.583333333333333em")
+                .setHeight("2em")
+                .setCaption("")
+                .setHAlign("left")
+                .setFontSize("18px")
+            );
+            
+            host.xui_ui_block6.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label4")
+                .setLeft("12.5em")
+                .setTop("5em")
+                .setWidth("5.916666666666667em")
+                .setCaption("")
+                .setHAlign("left")
+                .setFontSize("18px")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
